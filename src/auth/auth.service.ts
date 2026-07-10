49dc2d972dbc45d3ff1dd208ba89951faecf1a03
Montassar Kolsi <125766363+MontassarKolsi@users.noreply.github.com>
2026-07-10 11:19:32 +0200

    feat: setting up docker+prisma

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(dto: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+    return this.prisma.user.create({
+      data: {
+        email: dto.email,
+        password: hashedPassword,
+      },
+    });
+  }
+}
